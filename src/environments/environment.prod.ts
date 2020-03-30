139d1409a6bf7897803cbe17bbe8f40390738609
avmaxim <avmax19@gmail.com>
2020-03-30 08:40:21 +0300

    Added tv config fix in ng serve and build. Setup environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+import pkg from '../../package.json';
+
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'localhost:4200',    // TODO: Replace for a production base url when the app is deployed
+  version: pkg.version,
 };
