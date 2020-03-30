62c50d430135527bf73418b238f1bfa062fe4e21
avmaxim <avmax19@gmail.com>
2020-03-30 14:43:27 +0300

    Added Home basic component. Updated app.module to contain Orsay, Netcast and AmazonFiretv Platform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { TVBaseModule } from '@alignedcode/tvbase';
 
 import { AppComponent } from './app.component';
 import { HttpGateway } from './services/http-gateway.service';
 import { EnvironmentService } from './services/environment.service';
-import { PlatformService, WebOsPlatform, TizenPlatform, DesktopPlatform} from '@alignedcode/tvbase';
+import {
+  TVBaseModule,
+  PlatformService,
+  WebOsPlatform,
+  TizenPlatform,
+  DesktopPlatform,
+  NetcastPlatform,
+  OrsayPlatform,
+  AmazonFireTvPlatform
+} from '@alignedcode/tvbase';
 import { HttpClientModule } from '@angular/common/http';
 import { routing } from './app-routing';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -33,6 +43,9 @@ export class AppModule {
   constructor(platformService: PlatformService) {
     platformService.add(WebOsPlatform);
     platformService.add(TizenPlatform);
+    platformService.add(NetcastPlatform);
+    platformService.add(OrsayPlatform);
+    platformService.add(AmazonFireTvPlatform);
     platformService.add(DesktopPlatform);
-}
+  }
 }
