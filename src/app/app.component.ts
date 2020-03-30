39e205121856b5d01b95cd2b4e3c3c5372151838
avmaxim <avmax19@gmail.com>
2020-03-30 09:14:43 +0300

    Updated app compoent. Implemented app componetn ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { provideNavigatable, KeysService, NavigationService, PlatformService, NavigatableComponent } from '@alignedcode/tvbase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [provideNavigatable(AppComponent)]
 })
-export class AppComponent {
+export class AppComponent extends NavigatableComponent {
   title = 'Fitness';
+
+  constructor(
+    elementRef: ElementRef,
+    keysService: KeysService,
+    private navigationService: NavigationService
+  ) {
+    super(elementRef, null, keysService);
+    this.navigationService.setRoot(this);
+  }
 }
